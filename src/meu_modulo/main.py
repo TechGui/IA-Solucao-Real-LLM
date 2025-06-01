13e7942315550ad97b166b1363bde7a7a6d37627
João Vitor <joaovitordolinski@gmail.com>
2025-06-01 18:20:06 -0300

    Adding YOLOv5 identification model

--- src/meu_modulo/main.py
+++ src/meu_modulo/main.py
@@ -1,36 +1,8 @@
 import sys
 
-from .algoritmos.gemini_integration import GeminiClient
-from .utils import parse_args
+from algoritmos import detectar_vacas
 
-"""
-  Exemplo de utilização:
-  uv run ia-m-uv -p 'prompt aqui'
-  
-  O arquivo main.py será chamado, nele a utilização do 'utils' é vista, além de ser possível a partir dele chamar outros algoritmos, até mesmo passar algum argumento texto:
-  
-  -p OU --problema 'prompt vai aqui'
-"""
+if __name__ == "main":
+    video_path = "src/meu_modulo/vacas.mp4"
+    detectar_vacas.detectar_vacas(video_path)
 
-
-def main() -> None:
-    print("Meu projeto!")
-    args = parse_args()
-
-    try:
-        problema = args.problema
-        print(f"Problema: {problema}")
-
-        print("--- Teste de Geração de Texto Padrão ---")
-        gemini_client = GeminiClient(api_key="chave-de-api", model_name="gemma-3-27b-it")
-        response_text = gemini_client.generate_response(problema)
-        print(f"Resposta: {response_text}\n")
-
-        return 0
-    except Exception as e:
-        print(f"Erro: {e}")
-        return 1
-
-
-if __name__ == "__main__":
-    sys.exit(main())
