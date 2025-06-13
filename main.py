1201c6364fd4cc6ae71b1463c132e1ca02e10e43
TechGui <guilhermedrsilva7@gmail.com>
2025-06-12 22:16:23 -0300

    feat: add text to voice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from integracoes.huggingface_api import enviar_mensagem_hf
+from integracoes.texto_para_voz import texto_para_voz
 
 print("Cubô está ao seu serviço. Digite 'sair' para encerrar.\n")
 
@@ -8,3 +9,4 @@ while True:
         break
     resposta = enviar_mensagem_hf(entrada)
     print("Cubô:", resposta)
+    texto_para_voz(resposta)
